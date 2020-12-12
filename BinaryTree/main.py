d7c1596f3b5b4588fcfaccb9c7ad7136a17d9196
Dmitry Vengerov <hardbeat34@gmail.com>
2020-12-13 02:11:58 +0300

    Add btree

---
+++ BinaryTree/main.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.p = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, k):
+        t = Node(k)
+        parent = None
+        node = self.root
+
+        while node != None:
+            parent = node
+            if node.key > t.key:
+                node = node.left
+            else:
+                node = node.right
+        t.p = parent
+
+        if parent == None:
+            self.root = t
+        elif t.key < parent.key:
+            parent.left = t
+        else:
+            parent.right = t
+        return t
+
+if __name__ == "__main__":
+    bTree = BinaryTree();
+
+    bTree.insert('test')
+    bTree.insert('test1')
+    bTree.insert('test2')
+
+    print(bTree)
+    pass
+   
